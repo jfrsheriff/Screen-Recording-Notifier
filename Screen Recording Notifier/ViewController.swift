b53a36e24a3a6e495514eee163ee40e8250051d4
Jaffer Sheriff <jaffersheriff.u@zohocorp.com>
2019-10-29 12:55:43 +0530

    Initial Commit

---
+++ Screen Recording Notifier/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Screen Recording Notifier
+//
+//  Created by Jaffer Sheriff U on 29/10/19.
+//  Copyright © 2019 Jaffer Sheriff U. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Screen Recording NotifierTests/Screen_Recording_NotifierTests.swift
@@ -0,0 +1,34 @@
+//
+//  Screen_Recording_NotifierTests.swift
+//  Screen Recording NotifierTests
+//
+//  Created by Jaffer Sheriff U on 29/10/19.
+//  Copyright © 2019 Jaffer Sheriff U. All rights reserved.
+//
+
+import XCTest
+@testable import Screen_Recording_Notifier
+
+class Screen_Recording_NotifierTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
